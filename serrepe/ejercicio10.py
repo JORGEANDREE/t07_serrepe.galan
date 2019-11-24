cee70c50b8556f40a7954a3bbb774e7ca639f747
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-24 12:54:24 -0500

    se agrego el 5to ejercicio de iterador

---
+++ serrepe/ejercicio10.py
@@ -0,0 +1,20 @@
+import os
+# Decodificar mensaje encriptado
+# k = corre
+# l = por
+# m = tu
+# n = vida
+msg=os.sys.argv[1]
+#for
+for letra in msg:
+    if letra == "k":
+        print("corre")
+    if letra == "l":
+        print("por")
+    if letra == "m":
+        print("tu")
+    if letra == "n":
+        print("vida")
+#fin_iterador
+
+print("Fin del bucle")
