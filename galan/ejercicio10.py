663cc835808f6679bbd23ca2f7421dfff211ef76
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-24 14:28:08 -0500

    se agrego el 5to ejercicio de iterador

---
+++ galan/ejercicio10.py
@@ -0,0 +1,24 @@
+#Decodificar mensaje encriptado
+import os
+# K = Buenos dias
+# L = Estan coordinarmente invitados
+# O = A mi reunion
+# S = El dia de mañana
+# X = Gracias
+msg= os.sys.argv[1].upper()
+
+for letra in msg:
+    if letra == "K" :
+        print("Buenos dias")
+    if letra == "L" :
+        print("Estan coordinarmente invitados")
+    if letra == "O" :
+        print("A mi reunion")
+    if letra == "S" :
+        print("El dia de mañana")
+    if letra == "X":
+        print("Gracias")
+
+#fin_iterador
+
+print("fin del bucle"
