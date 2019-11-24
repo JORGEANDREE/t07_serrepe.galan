a1598ccd10a6c9627839ed93b1ff9e83b3d60605
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-24 14:31:43 -0500

    se agrego el 4to ejercicio de iterador en rango

---
+++ galan/ejercicio14.py
@@ -0,0 +1,9 @@
+#Imprimir la multiplicacion por 15 de los x primeros numeros
+import os
+x=int(os.sys.argv[1])
+
+for i in range(x):
+    print((i+1)*15)
+#fn iterador con rango
+
+print("fin del bucle")
