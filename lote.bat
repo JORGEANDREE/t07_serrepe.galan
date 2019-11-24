03137c1174ebfb9c5def11f2ccbb9c0af1c3a14c
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-24 14:06:19 -0500

    se agrego el lote.bat

---
+++ lote.bat
@@ -0,0 +1,30 @@
+python serrepe/ejercicio1.py "lunes"
+python serrepe/ejercicio1.py "martes"
+python serrepe/ejercicio1.py "miercoles"
+python serrepe/ejercicio2.py "si"
+python serrepe/ejercicio2.py "no"
+python serrepe/ejercicio2.py "si"
+python serrepe/ejercicio3.py 1000.2 3.4
+python serrepe/ejercicio3.py 1032.2 2.4
+python serrepe/ejercicio4.py 100.6
+python serrepe/ejercicio4.py 65.6
+python serrepe/ejercicio4.py 70.0
+python serrepe/ejercicio5.py "entel"
+python serrepe/ejercicio5.py "claro"
+python serrepe/ejercicio5.py "movistar"
+python serrepe/ejercicio6.py "ABCD"
+python serrepe/ejercicio6.py "DCBA"
+python serrepe/ejercicio6.py "BADC"
+python serrepe/ejercicio7.py "EFGH"
+python serrepe/ejercicio7.py "FEGH"
+python serrepe/ejercicio7.py "HGEF"
+python serrepe/ejercicio8.py "TYUP"
+python serrepe/ejercicio8.py "PUYT"
+python serrepe/ejercicio8.py "UYTP"
+python serrepe/ejercicio9.py "EFGH"
+python serrepe/ejercicio9.py "FGHE"
+python serrepe/ejercicio9.py "EEEE"
+python serrepe/ejercicio10.py "klmn"
+python serrepe/ejercicio10.py "kkkk"
+python serrepe/ejercicio10.py "llll"
+
---
+++ loteerr.bat
@@ -0,0 +1,29 @@
+python serrepe/ejercicio1.py "assadsad" "lunes"
+python serrepe/ejercicio1.py "sasdas" "martes"
+python serrepe/ejercicio1.py "ddsadasd" "jueves"
+python serrepe/ejercicio2.py "nel" "no"
+python serrepe/ejercicio2.py "yes" "si"
+python serrepe/ejercicio2.py "lo" "si"
+python serrepe/ejercicio3.py -1.2 -1.3
+python serrepe/ejercicio3.py -3.22 -1.4
+python serrepe/ejercicio4.py -23.4
+python serrepe/ejercicio4.py -55.6
+python serrepe/ejercicio4.py -10.0
+python serrepe/ejercicio5.py "sasdas"
+python serrepe/ejercicio5.py "gmail"
+python serrepe/ejercicio5.py "hotmail"
+python serrepe/ejercicio6.py "cvcc"
+python serrepe/ejercicio6.py "qwdwq"
+python serrepe/ejercicio6.py "safas"
+python serrepe/ejercicio7.py "qwewq"
+python serrepe/ejercicio7.py "sadd"
+python serrepe/ejercicio7.py "qwww"
+python serrepe/ejercicio8.py "eeee"
+python serrepe/ejercicio8.py "rrrr"
+python serrepe/ejercicio8.py "tttt"
+python serrepe/ejercicio9.py "yyyy"
+python serrepe/ejercicio9.py "yyyy"
+python serrepe/ejercicio9.py "Eqqq"
+python serrepe/ejercicio10.py "pppp"
+python serrepe/ejercicio10.py "aaaa"
+python serrepe/ejercicio10.py "bbbb"
