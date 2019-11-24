0e7c1fe35ea2d57be3e4e656e9df4fc5509ada4f
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-24 12:42:29 -0500

    se agrego el 3er ejercicio de iterador

---
+++ serrepe/ejercicio8.py
@@ -0,0 +1,20 @@
+import os
+# Decodificar mensaje encriptado
+# T = LA
+# Y = PROGRAMACION
+# U = ES LO
+# P= MEJOR
+msg=os.sys.argv[1]
+#for
+for letra in msg:
+    if letra == "T":
+        print("LA")
+    if letra == "Y":
+        print("PROGRAMACION")
+    if letra == "U":
+        print("ES LO")
+    if letra == "P":
+        print("MEJOR")
+#fin_iterador
+
+print("Fin del bucle")
