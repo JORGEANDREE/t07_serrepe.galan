2f89c74b791b4a5dd40f812eebdc0f4008abc73c
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-24 14:30:06 -0500

    se agrego el 2do ejercicio de iterador en rango

---
+++ galan/ejericio12.py
@@ -0,0 +1,9 @@
+#Imprimir la multiplicacion por 7 de los x primeros numeros
+import os
+x=int(os.sys.argv[1])
+
+for i in range(x):
+    print((i+1)*7)
+#fn iterador con rango
+
+print("fin del bucle")
