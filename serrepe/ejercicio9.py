a4c52289c36809454e596d5005930d8557d4e916
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-24 12:46:43 -0500

    se agrego el 4to ejercicio de iterador

---
+++ serrepe/ejercicio9.py
@@ -0,0 +1,20 @@
+import os
+# Decodificar mensaje encriptado
+# E = viva
+# F = el
+# G = peru
+# H = !
+msg=os.sys.argv[1]
+#for
+for letra in msg:
+    if letra == "E":
+        print("viva")
+    if letra == "F":
+        print("el")
+    if letra == "G":
+        print("peru")
+    if letra == "H":
+        print("!")
+#fin_iterador
+
+print("Fin del bucle")
