e95c1ac8f36637f26503b60fe3f76c17c1e27c2c
JORGEANDREE <57020796+jorgeandree@users.noreply.github.com>
2019-11-24 14:15:10 -0500

    se agrego el 1er ejercicio de while

--- galan/ejercicio1.py
+++ galan/ejercicio1.py
@@ -7,7 +7,7 @@ nota1 = float (os.sys.argv [ 1 ])
 nota2 = float (os.sys.argv [ 2 ])
 nota3 = float (os.sys.argv [ 3 ])
 promedio = (nota1 + nota2 + nota3) / 3
-promedio_invalido = (promedio < 0  o promedio > 20 )
+promedio_invalido = (promedio < 0  or promedio > 20 )
 
 # while
 while (promedio_invalido ==  True ):
@@ -15,7 +15,7 @@ while (promedio_invalido ==  True ):
     nota2 = float ( input ( " pedir la nota2: " ))
     nota3 =  float ( input ( " pedir la nota3: " ))
     promedio = (nota1 + nota2 + nota3) / 3
-    promedio_invalido = (promedio <  0  o promedio >  20 )
+    promedio_invalido = (promedio <  0  or promedio >  20 )
 # fin_del while
 print ( " fin del bucle " )
 print ( " promedio: " , promedio)
